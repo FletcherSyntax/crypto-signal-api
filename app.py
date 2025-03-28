465d20162c3cc4a4a923cf1f9dfbc2a4c706705f
FletcherSyntax <jeff@syntaxproductions.net>
2025-03-28 13:40:32 -0400

    Update app.py
    Added port handling at the end of the script

--- app.py
+++ app.py
@@ -34,3 +34,9 @@ def signals_json():
         return response
     except FileNotFoundError:
         return jsonify({"error": "signals.json not found"}), 404
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
